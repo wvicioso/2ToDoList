7b10124552d6b016d1c717c241fa83ebf0076bf9
Amanda Hinchey <akhinchey@gmail.com>
2016-09-24 19:49:12 -0400

    Add task views

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -1,4 +1,12 @@
 get '/lists' do 
   @lists = List.all 
+  erb :'list/index'
+end
+
+get '/lists/new' do 
+end
+
+get '/lists/:list_id' do
+  @list = List.find_by(id: params[:list_id])
   erb :'list/show'
 end
