071c931f1bc03ad3b8b30c12242912771c30e1ac
Amanda Hinchey <akhinchey@gmail.com>
2016-09-24 20:11:17 -0400

    Add login

---
+++ app/controllers/login.rb
@@ -0,0 +1,14 @@
+get '/login' do 
+  erb :'login'
+end
+
+post '/login' do 
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/lists"
+  else
+    @error = "Invalid login, please try again"
+    erb :'login'
+  end
+end
