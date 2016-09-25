76b9bb9fbd118439653800c688fe465c4519e3b1
perohrstrom <per.k.ohrstrom@gmail.com>
2016-09-25 12:39:57 -0400

    Carryover Helper Methods and Create Log In Form

--- app/controllers/lists.rb
+++ app/controllers/lists.rb
@@ -4,9 +4,14 @@ end
 
 post '/lists' do
   list = List.create(params[:list])
-  if list.save?
-    
+  if list.save
+    if request.xhr?
+    erb :'/lists/_list', locals: { list: list }, layout: false
+    else
+      redirect '/'
+    end
   else
+    binding.pry
     @errors = list.errors.full_messages
     erb :'index'
   end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,19 @@
 get '/' do
+  @lists = List.all
   erb :'index'
+end
+
+post '/users' do
+  user = User.create(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    if request.xhr?
+      "hello"
+    else
+    redirect '/'
+    end
+  else
+    @errors = user.errors.full_messages
+    erb :'/sessions/new'
+  end
 end
