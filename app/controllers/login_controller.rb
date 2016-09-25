8922141c3a3d047c275c2a62d02fb080815bc448
Austin Lanari <email": null>
2016-09-25 10:58:44 -0400

    Get login working with errors

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,3 +1,14 @@
 get '/login/new' do
   erb :'login/new'
 end
+
+post '/login' do
+  @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = ['Incorrect email or password']
+    erb :'login/new'
+  end
+end
