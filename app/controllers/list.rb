dfa31e5049424cd74010ea0dc40e81f240b1ba15
Amanda Hinchey <akhinchey@gmail.com>
2016-09-24 23:38:09 -0400

    Add new list form

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -4,6 +4,17 @@ get '/lists' do
 end
 
 get '/lists/new' do 
+  erb :'list/new'
+end
+
+post '/lists/new' do 
+  @list = List.new(user_id: current_user.id, title: params[:title])
+  if @list.save
+    redirect "lists/#{@list.id}"
+  else
+    @error = "Please add a title"
+    erb :'list/new'
+  end
 end
 
 get '/lists/:list_id' do
