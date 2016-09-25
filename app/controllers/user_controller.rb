66551d1b823ed8bb517b7709f354bf172cf8325c
Neel Gupta <neelgupta2@gmail.com>
2016-09-25 17:26:53 -0400

    Add user registration routes, form partial, simple css.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,25 @@
 # Display a list of all users
 get '/users' do
+  @users = User.all
+
+  erb :'users/index'
 end
 
 # Form to create new user
 get '/users/new' do
+  erb :'users/_new_user'
 end
 
 # Post after new user is created
 post '/users' do
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/users'
+  end
 end
 
 # Specific user
