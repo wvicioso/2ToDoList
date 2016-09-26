cadbf0131032c007dd3df04514d2332de997f5cc
edwincarbajal <exaviercarbajal@gmail.com>
2016-09-25 20:41:21 -0400

    create logout/login

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,3 +8,23 @@ post '/users' do
     erb :index
   end
 end
+
+get '/login' do
+  erb :'users/login'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @errors = ['Invalid username or password']
+    erb :'users/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
