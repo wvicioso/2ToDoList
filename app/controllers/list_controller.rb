aa56249df5908ac9a452b89888049f44aa6f3ec6
Austin Lanari <email": null>
2016-09-27 11:31:39 -0400

    Get list titles update

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -31,3 +31,13 @@ put '/lists/:list_id/tasks'do
   completed_task_ids.each { |task_id| Task.update(task_id, status: true) }
   erb :'lists/show'
 end
+
+get '/lists/:list_id/edit' do
+  @list = TodoList.find_by(id: params[:list_id])
+  erb :'lists/edit'
+end
+
+put '/lists/:list_id' do
+  TodoList.update(params[:list_id], name: params[:name])
+  redirect "/lists/#{params[:list_id]}"
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :todo_list
 
   validates :description, presence: true
+
+  def complete?
+    self.status
+  end
 end
