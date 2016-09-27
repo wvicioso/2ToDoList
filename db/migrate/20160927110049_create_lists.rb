641bd95e9b4ae3d9771c78a986021d54704dc606
Angel Chen & Thomas Graves <pair+angel-chen.thomaslogangraves@devbootcamp.com>
2016-09-27 11:08:06 -0400

    Create migrations for user, list and tasks

--- db/migrate/20160927110049_create_lists.rb
+++ db/migrate/20160927110049_create_lists.rb
@@ -1,4 +1,10 @@
 class CreateLists < ActiveRecord::Migration
   def change
+    create_table :lists do |t|
+      t.string :name, null: false
+      t.references :users
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20160927110054_create_tasks.rb
+++ db/migrate/20160927110054_create_tasks.rb
@@ -1,4 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
+    create_table :tasks do |t|
+      t.string :description, null: false
+      t.references :lists
+
+      t.timestamps
+    end
   end
 end
