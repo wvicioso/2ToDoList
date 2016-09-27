58c366d9c02bc7c258c63a384cf774eb77e34503
Kimberly Suazo <email": null>
2016-09-27 16:44:49 -0400

    Add over_due? method to class Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :list_id, presence: true 
+
+  def task_overdue? 
+    if self.due_date
+      self.due_date < Time.now 
+    end   
+  end 
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,8 @@ u2 = User.create!(username: "bono", email: "bono@me.com", password:"password")
 
 list2= List.create!(title:"Upcoming", user_id: u2.id)
 
-task1 = Task.create!(description:"Sign deal", list_id: list2.id)
+d = Date.parse('17/9/2015')
+
+task1 = Task.create!(description:"Sign deal", list_id: list2.id, due_date: d)
 task2 = Task.create!(description:"Meet with band", list_id: list2.id)
+
