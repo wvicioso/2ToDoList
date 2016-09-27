f8816c92f37197dcbcef2bbd26237fd4f2c27b93
Austin Lanari <email": null>
2016-09-27 09:48:31 -0400

    Get list creation and association with a user working

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,3 +2,22 @@ get '/lists' do
   @lists = TodoList.all
   erb :'lists/index'
 end
+
+get '/lists/new' do
+  erb :'lists/new'
+end
+
+post '/lists' do
+  @list = TodoList.new(params[:todolist])
+  if @list.save
+    current_user.todo_lists << @list
+    redirect "/lists/#{@list.id}"
+  else
+    @errors = "Your list needs a title!"
+    erb :'lists/new'
+  end
+end
+
+get '/lists/:id' do
+  erb :'lists/show'
+end
