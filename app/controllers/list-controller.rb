80b08aeda3d11b1c4fdff7efe4d79709a6d1077f
Kimberly Suazo <email": null>
2016-09-27 12:30:52 -0400

    Add create list functionality

--- app/controllers/list-controller.rb
+++ app/controllers/list-controller.rb
@@ -2,3 +2,19 @@ get '/lists' do
   @lists = List.all 
   erb :'lists/index'
 end   
+
+get '/lists/:id' do 
+  @list = List.find_by(id: params[:id])
+  erb :'/lists/show'
+end 
+
+post '/lists/new' do
+  list = List.new(title: params[:title], user_id: current_user.id)
+  if list.save
+    redirect '/lists'
+  else
+    @errors = list.errors.full_messages
+    @lists = List.all
+    erb :'/lists/index', locals: {errors: @errors}
+  end   
+end   
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user 
   has_many :tasks
+
+  validates :title, :user_id, presence:true 
 end
