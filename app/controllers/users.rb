a7ef558fc9f85c8ccb3d57eea4337b045d86f72c
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-09-27 14:59:08 -0400

    Create user views, errors partial and session helpers.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,25 +1,30 @@
 get '/register' do
-  if logged_in?
-    direct '/'
-  else
-    erb :'users/new'
-  end
+  # if logged_in?
+  #   direct '/'
+  # else
+  #   erb :'users/new'
+  # end
+  erb :'users/register'
 end
 
-post '/register' do
+post '/users' do
   user = User.new(params[:user])
   if user.save
     session[:user_id] = user.id
     redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'users/register'
   end
 end
 
 get '/login' do
-  if logged_in?
-    redirect '/'
-  else
+  # if logged_in?
+  #   redirect '/'
+  # else
+  #   erb :'users/login'
+  # end
     erb :'users/login'
-  end
 end
 
 post '/login' do
@@ -34,8 +39,10 @@ post '/login' do
 end
 
 get '/logout' do
-  if logged_in?
-    session.clear
-  end
-    redirect '/'
+  # if logged_in?
+  #   session.clear
+  # end
+  #   redirect '/'
+  session.clear
+  redirect '/'
 end
