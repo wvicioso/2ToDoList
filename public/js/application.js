5dee509fbb1721e55139f74434b41361e14b002f
neelgupta2 <neelgupta2@gmail.com>
2016-09-27 23:01:35 -0400

    Work on AJAX delete, not functioning.

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-  
+  $('.container').on('click', '#deleteButton', function() {
+    
+    // event.preventDefault();
+
+    var url = $(this).attr('action');
+    // debugger;
+
+    $.ajax({
+      url: url,
+      method: 'DELETE'
+    }).done(function(response) {
+
+      // debugger;
+    })
+  });
 });
